cdb1e567150eebcdda9c7255b8839e91014a79ce
Carl du Preez <carldupreez4@gmail.com>
2025-07-01 18:48:31 +0000

    Clean up of controllers

--- src/TimeManagemetApp.Models/Models/WeatherForecast.cs
+++
@@ -1,7 +0,0 @@
-namespace TimeManagemetApp.Models.Models
-{
-    public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-    {
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-    }
-}
